ca4c9f631ecd95e221c275c3e00d956eabbe59b9
Jakes du Preez <dupreez.jakes@gmail.com>
2018-08-08 17:57:35 +0200

    Add rest service call

--- smack/Utilities/Constants.swift
+++ smack/Utilities/Constants.swift
@@ -8,7 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = "http://localhost:3005/v1/"
+let URL_ACCOUNT_REGISTER = "\(BASE_URL)account/register"
+let URL_ACCLOUNT_LOGIN = "\(BASE_URL)account/login"
+let URL_USER_ADD = "\(BASE_URL)user/add"
+
 //Seques
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND_TO_CHANNEL = "unwindToChannel"
+
+//UserDefaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
