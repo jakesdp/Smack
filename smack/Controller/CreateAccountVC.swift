fa59aeb0399c0c891fadd187b8a6774d430e7b6d
Jakes du Preez <dupreez.jakes@gmail.com>
2018-08-12 11:09:45 +0200

    Create user

--- smack/Controller/CreateAccountVC.swift
+++ smack/Controller/CreateAccountVC.swift
@@ -16,6 +16,8 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,13 +26,25 @@ class CreateAccountVC: UIViewController {
     }
 
     @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = usernameTxt.text, usernameTxt.text != "" else {return }
         guard let email = emailTxt.text, emailTxt.text != "" else {return}
         guard let password = passTxt.text, passTxt.text != "" else {return}
         
         AuthService.instance.registerUser(email: email, password: password) { (success) in
             if success {
+                print("Successfully registered user \(AuthService.instance.userEMail)")
                 AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
-                    print("Successfully logged in \(AuthService.instance.userEMail) with token \(AuthService.instance.authToken)")
+                    if(success) {
+                        print("Successfully logged in \(AuthService.instance.userEMail) with token \(AuthService.instance.authToken)")
+                        AuthService.instance.createUSer(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (succes) in
+                            if success {
+                                print("Successfully created user \(UserDataService.instance.name): Avatar name \(UserDataService.instance.avatarName)")
+                                self.performSegue(withIdentifier: UNWIND_TO_CHANNEL, sender: nil)
+                            } else {
+                                
+                            }
+                        })
+                    }
                 })
             }
         }
--- smack/Services/AuthService.swift
+++ smack/Services/AuthService.swift
@@ -87,4 +87,46 @@ class AuthService{
             }
         }
     }
+    
+    func createUSer(name: String, email: String, avatarName: String, avatarColor: String, completion: @escaping CompletionHandler){
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        
+        let header = [
+            "Authorization": "Bearer \(AuthService.instance.authToken)",
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON { (response) in
+            if response.result.error == nil {
+                
+                guard let data = response.data else {return}
+                do{
+                    let json = try JSON(data: data)
+                       let id = json["_id"].stringValue
+                       let name = json["name"].stringValue
+                       let email = json["email"].stringValue
+                       let avatarName = json["avatarName"].stringValue
+                       let avatarColor = json["avatarColor"].stringValue
+                    
+                    UserDataService.instance.setUserData(id: id, avatarColor: avatarColor, avatarName: avatarName, email: email, name: name)
+                    completion(true)
+                }catch{
+                    completion(false)
+                    debugPrint(error as Any)
+                }
+                
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+        
+    }
 }
